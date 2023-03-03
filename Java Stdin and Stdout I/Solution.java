a4a305b3f63561367b14c50fa18c7070fae70b8f
Berkan <berkansahanofficial@gmail.com>
2023-03-03 23:27:32 +0300

    Java Stdin and Stdout I

---
+++ Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        int third = scanner.nextInt();
+        scanner.close();
+        
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+    }
+}
