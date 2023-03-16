358ab80306e01d279202b1b28c70aead8210e008
Berkan <berkansahanofficial@gmail.com>
2023-03-16 23:59:21 +0300

    Update Solution.java

--- Java Int to String/Solution.java
+++ Java Int to String/Solution.java
@@ -5,8 +5,12 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        String s = Integer.toString(n);
-        System.out.println("Good job");
+        scanner.close();
+        if (n <= 100 && n >= -100) {
+            String s = Integer.toString(n);
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer");
+        }
     }
-
 }
