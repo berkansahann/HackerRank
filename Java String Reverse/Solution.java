42f803c475ee7b03b5d4e81e1a2b64aac76907fd
Berkan Şahan <55889142+berkansahann@users.noreply.github.com>
2023-04-12 19:52:49 +0300

    Update Solution.java

--- Java String Reverse/Solution.java
+++ Java String Reverse/Solution.java
@@ -4,6 +4,16 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        String straight = scanner.nextLine();
+        scanner.close();
+        StringBuilder reversed = new StringBuilder(straight);
+        reversed.reverse();
+        if(straight.equals(reversed.toString())){
+            System.out.println("Yes");
+        }
+        else{
+        System.out.println("No");
+        }
     }
 }
