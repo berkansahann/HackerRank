96eb8297953fee978b7ce5e21d62aeb1ed762a19
Berkan Şahan <55889142+berkansahann@users.noreply.github.com>
2023-04-12 22:35:09 +0300

    Create Solution.java

---
+++ Java String Tokens/Solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        scanner.close();
+        s = s.trim();
+        if (s.isEmpty()) {
+            System.out.println("0");
+        } else {
+            String[] arrOfStr = s.split("[ !,?^&*`$@#$`//._'@]+");
+            System.out.println(arrOfStr.length);
+            for (String a : arrOfStr) {
+                System.out.println(a);
+            }
+        }
+    }
+}
