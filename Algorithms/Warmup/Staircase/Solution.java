58338296c433c2bd8737c012066b3e741d914380
Berkan Sahan <berkansahanofficial@gmail.com>
2023-05-26 10:49:35 +0300

    new solution

---
+++ Algorithms/Warmup/Staircase/Solution.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n) {
+        for(int c = 1; c <= n; c ++){
+            for(int spaceCounter = c; spaceCounter < n ; spaceCounter++){
+                System.out.print(" ");
+            }
+            for(int starCounter = c; starCounter > 0; starCounter--){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.staircase(n);
+
+        bufferedReader.close();
+    }
+}
